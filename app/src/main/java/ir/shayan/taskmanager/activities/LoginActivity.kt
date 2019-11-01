616ae3bd7b6f99028ce48705732fb9bb5488ea58
shayan <shayan4shayan@gmail.com>
2019-11-01 16:12:27 -0700

    login activity created.

---
+++ app/src/main/java/ir/shayan/taskmanager/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package ir.shayan.taskmanager.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.shayan.taskmanager.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
