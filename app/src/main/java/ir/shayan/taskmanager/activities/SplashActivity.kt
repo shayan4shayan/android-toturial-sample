6b46b441014da13ac72c3ef6d726e25e9b8a8778
shayan <shayan4shayan@gmail.com>
2019-11-01 17:17:11 -0700

    transition between splash and login added.

--- app/src/main/java/ir/shayan/taskmanager/activities/SplashActivity.kt
+++ app/src/main/java/ir/shayan/taskmanager/activities/SplashActivity.kt
@@ -1,7 +1,13 @@
 package ir.shayan.taskmanager.activities
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.transition.Explode
+import android.transition.Transition
+import android.view.Window
 import ir.shayan.taskmanager.R
 import ir.shayan.taskmanager.isLogin
 import ir.shayan.taskmanager.startSomeActivity
@@ -11,10 +17,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        if (isLogin()){
-            startSomeActivity(MainActivity::class.java)
-        } else {
-            startSomeActivity(LoginActivity::class.java)
-        }
+        Handler().postDelayed({
+            if (isLogin()){
+                startSomeActivity(MainActivity::class.java)
+            } else {
+                startActivity(Intent(this,LoginActivity::class.java),
+                    ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+            }
+        },2000)
     }
 }
